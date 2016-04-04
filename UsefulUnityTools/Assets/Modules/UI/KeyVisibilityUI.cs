bc903ee5c17872993c09cb4b4a805836d53136e0
Gordon Alexander MacPherson <gordon.a.macpherson@gmail.com>
2016-04-04 15:23:31 +0100

    Added KeyVisibilityUI for handling the Console UI element, but it could be used in other places.
    Specify which key should be down to toggle visibility.

---
+++ UsefulUnityTools/Assets/Modules/UI/KeyVisibilityUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// UI Element visiblity
+/// Press the specified key / input button to toggle the object visibility
+/// </summary>
+public class KeyVisibilityUI : MonoBehaviour {
+    /// <summary>
+    /// Show the UI Element based on this 'input' from unity.
+    /// </summary>
+    public string mVisibleInput = "ShowConsole";
+
+    /// <summary>
+    /// The UI Element which we toggle.
+    /// </summary>
+    public GameObject mUIElement;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown(mVisibleInput))
+        {
+            // Invert the visibility state
+            mUIElement.SetActive(!mUIElement.activeSelf);
+        }
+	}
+}
